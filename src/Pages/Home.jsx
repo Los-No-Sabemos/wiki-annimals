6caf9ce6c2b1b81c2d89f0be190f8d6a29c08a7a
Eliana Depietri <eliana.depietri@gmail.com>
2025-05-03 23:59:52 +0200

    add API creation + axios

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,10 @@
+import AnimalList from "../components/animalList"   
+
 export default function Home(){
    return(
    <>
     <p>THIS IS MY HOME FOR THE NEXT WEEK</p>
+    <AnimalList/>
     </>
     ) 
 }
--- src/components/AnimalList.jsx
+++ src/components/AnimalList.jsx
@@ -1,6 +1,23 @@
+import axios from "axios"
+import { useEffect, useState } from "react";
+
 export default function AnimalList () {
+
+    const [animals, setAnimals] = useState();
+    
+    const API_URL= 'https://animal-wiki-47b79-default-rtdb.europe-west1.firebasedatabase.app/'
+
+    useEffect()
+
+    
+    
     return (
-     <></>
+     <>
+    
+    
+
+
+     
+     </>
     )
 }
-
