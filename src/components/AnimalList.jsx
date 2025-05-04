f60ae66c52c73337f70fa7eef6014a45d245448a
Florian Wunsch <Florian95.wunsch@gmail.com>
2025-05-04 20:25:22 +0200

    api now displays all our data

--- src/components/AnimalList.jsx
+++ src/components/AnimalList.jsx
@@ -28,10 +28,20 @@ export default function AnimalList() {
     }, []);
 
     return (
-        <>
-            {animals.map((animal) => (
-                <div key={animal.id}>{animal.name}</div>
-            ))}
-        </>
+        <div className="animal-card-grid">
+        {animals.map((animal) => (
+          <div className="animal-card" key={animal.id}>
+            <img src={animal.image_Url} alt={animal.name} className="animal-image" />
+            <div className="animal-card-body">
+              <h2>{animal.name}</h2>
+              <p>Description: {animal.description}</p>
+              <p>Habitat {animal.habitat}</p>
+              <p>Diet: {animal.diet}</p>
+              <p>Region: {animal.region}</p>
+              <p className="animal-fact"><strong>Fun Fact:</strong> {animal.fact}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     );
 }
