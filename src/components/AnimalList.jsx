75b3b0526d8f5d29a4f5daeee2c7dffc4d3ab1f1
Florian Wunsch <Florian95.wunsch@gmail.com>
2025-05-04 13:36:26 +0200

    working animal list and api get

--- src/components/AnimalList.jsx
+++ src/components/AnimalList.jsx
@@ -1,23 +1,37 @@
-import axios from "axios"
+import axios from "axios";
 import { useEffect, useState } from "react";
 
-export default function AnimalList () {
+export default function AnimalList() {
+    const [animals, setAnimals] = useState([]);
 
-    const [animals, setAnimals] = useState();
-    
-    const API_URL= 'https://animal-wiki-47b79-default-rtdb.europe-west1.firebasedatabase.app/'
+    const API_URL =
+        'https://animal-wiki-47b79-default-rtdb.europe-west1.firebasedatabase.app/animals.json';
 
-    useEffect()
+    useEffect(() => {
+        axios
+            .get(API_URL)
+            .then((response) => {
+                const animalData = response.data;
+                if (animalData) {
+                    const animalArr = Object.keys(animalData).map((id) => ({
+                        id,
+                        ...animalData[id],
+                    }));
+                    setAnimals(animalArr);
+                } else {
+                    setAnimals([]);
+                }
+            })
+            .catch((error) => {
+                console.log("No animals were found", error);
+            });
+    }, []);
 
-    
-    
     return (
-     <>
-    
-    
-
-
-     
-     </>
-    )
+        <>
+            {animals.map((animal) => (
+                <div key={animal.id}>{animal.name}</div>
+            ))}
+        </>
+    );
 }
