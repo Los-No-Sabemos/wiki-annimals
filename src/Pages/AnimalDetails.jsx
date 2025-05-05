b8821b3dfb7098a0ca7a7b86f05601afce252b8d
Florian Wunsch <Florian95.wunsch@gmail.com>
2025-05-05 20:58:29 +0200

    Add delete feature and update btn in animaldetail with delete validation

--- src/Pages/AnimalDetails.jsx
+++ src/Pages/AnimalDetails.jsx
@@ -12,35 +12,57 @@ export default function AnimalDetails() {
 
     useEffect(() => {
         axios
-          .get(`${API_URL}/animals/${id}.json`)
-          .then((response) => {
-            const animalData = response.data;
-            if (animalData) {
-              
-              setAnimal(animalData);
-            } 
-          })
-          .catch((error) => {
-            console.log("No animals were found", error);
-          });
-          console.log(setAnimal(id));
-      }, [id]);
-    
+            .get(`${API_URL}/animals/${id}.json`)
+            .then((response) => {
+                const animalData = response.data;
+                if (animalData) {
+                    setAnimal(animalData);
+                }
+            })
+            .catch((error) => {
+                console.log("No animals were found", error);
+            });
+        console.log(setAnimal(id));
+    }, [id]);
+
+    const handleDelete = () => {
+        const confirmed = window.confirm("Are you sure you want to Delete ? Eliana will be angry ");
+        if (!confirmed) return;
+        axios
+            .delete(`${API_URL}/animals/${id}.json`)
+            .then(() => {
+                alert("Animal deleted successfully!");
+                navigate("/");
+            })
+            .catch((error) => {
+                console.error("Error deleting animal", error);
+                alert("Failed to delete animal.");
+            });
+    };
+
+    const handleUpdate = () => {
+        navigate(`/UpdateAnimal/${id}`);
+    };
+
 
     return (
-        
-         <section key={animal.id} className="animal-slide" style={{ backgroundImage: `url(${animal.image_Url})` }}>
+
+        <section key={animal.id} className="animal-slide" style={{ backgroundImage: `url(${animal.image_Url})` }}>
             <div>
-              <h2>{animal.name}</h2>
-              <p>Description: {animal.description}</p>
-              <p>Habitat:{animal.habitat}</p>
-              <p>Diet: {animal.diet}</p>
-              <p>Region: {animal.region}</p>
-              <p className="animal-fact">Fun Fact: {animal.fact}</p>
+                <h2>{animal.name}</h2>
+                <p>Description: {animal.description}</p>
+                <p>Habitat:{animal.habitat}</p>
+                <p>Diet: {animal.diet}</p>
+                <p>Region: {animal.region}</p>
+                <p className="animal-fact">Fun Fact: {animal.fact}</p>
+                <div className="animal-buttons">
+                    <button onClick={handleUpdate}>Update</button>
+                    <button onClick={handleDelete}>Delete</button>
+                </div>
             </div>
-          </section>
-        
-      
+        </section>
+
+
 
     )
 
